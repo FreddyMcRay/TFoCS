f7c18e4c077dc941498f5b8d2dde08154b882c42
Evgenuy Petrushevski <evgenuypetrushevski@gmail.com>
2017-10-07 18:53:31 +0300

    ByteStaffing

---
+++ COM_to_COM with ByteStaffing/COM_to_COM/ByteStaffing.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM_to_COM
+{
+    class ByteStuffing
+    {
+        private const byte Flag = 0x7E;
+        private const byte Esc = 0x7D;
+        private const byte FlagCh = 0x5E;
+
+        public static byte[] Decode(byte[] data)
+        {
+            if (!SendValidation(data)) return null;
+
+            int dataLength = data.Length;
+            int count = 0;
+            for (int i = 0; i < dataLength; i++)
+                if (data[i] == Esc)
+                {
+                    count++;
+                    if (data[i + 1] == Esc)
+                        i++;
+                }
+
+            int newDataLength = dataLength - 4 - count;
+            byte[] newData = new byte[newDataLength];
+            int j = 0;
+            for (int i = 3; i < dataLength - 1; i++)
+            {
+                if (data[i] == Esc)
+                {
+                    if (data[++i] == FlagCh)
+                        newData[j++] = Flag;
+                    else
+                        newData[j++] = Esc;
+                }
+                else
+                {
+                    newData[j++] = data[i];
+                }
+            }
+            return newData;
+        }
+
+        public static byte[] Encode(byte[] data)
+        {
+            int newDataLength = GetNewDataLength(data);
+            byte[] newData = new byte[newDataLength];
+            InitializeBytes(ref newData);
+
+            int j = 3;
+            for (int i = 0; i < data.Length; i++)
+            {
+                switch (data[i])
+                {
+                    case Flag:
+                        newData[j++] = Esc;
+                        newData[j++] = FlagCh;
+                        break;
+                    case Esc:
+                        newData[j++] = Esc;
+                        newData[j++] = Esc;
+                        break;
+                    default:
+                        newData[j++] = data[i];
+                        break;
+                }
+            }
+            return newData;
+        }
+
+        private static bool SendValidation(byte[] data)
+        {
+            return data[1] == 0xFF && data[2] == 0x00;
+        }
+
+        private static void InitializeBytes(ref byte[] data)
+        {
+            data[0] = Flag;
+            data[1] = 0xFF;
+            data[2] = 0x00;
+            data[data.Length - 1] = Flag;
+        }
+
+        private static int GetNewDataLength(byte[] data)
+        {
+            int count = 0;
+            for (int i = 0; i < data.Length; i++)
+                if ((data[i] == Flag) || (data[i] == Esc))
+                    count++;
+            return data.Length + count + 4;
+        }
+
+    }
+}
---
+++ COM_to_COM with ByteStaffing/COM_to_COM/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COM_to_COM
+{
+    public partial class Form1 : Form
+    {
+        private SerialPortController serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPortController(ShowData);
+        }
+
+        private void canConnect(object sender, EventArgs e)
+        {
+            button1.Enabled = (textBox1.Text.Length != 0) && (comboBox1.SelectedIndex > -1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (button1.Text)
+            {
+                case "Connect": Connect(); break;
+                case "Disconnect": Disconnect(); break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort.SendData(Encoding.Default.GetBytes(richTextBox2.Text));
+            ShowData(richTextBox2.Text, true);
+            richTextBox2.Text = "";
+        }
+
+        private void ShowData(string data, bool sender)
+        {
+            richTextBox1.Text += ((sender ? "↑" : "↓") + " [" + DateTime.Now.ToString("HH:mm:ss tt") + "] " + data + "\n");
+        }
+
+        private void Connect()
+        {
+            serialPort.Connect(textBox1.Text, Int32.Parse(comboBox1.SelectedItem.ToString()));
+            button1.Text = "Disconnect";
+            textBox1.Enabled = false;
+            comboBox1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void Disconnect()
+        {
+            serialPort.Disconnect();
+            button1.Text = "Connect";
+            textBox1.Enabled = true;
+            comboBox1.Enabled = true;
+            button2.Enabled = false;
+        }
+    }
+}
